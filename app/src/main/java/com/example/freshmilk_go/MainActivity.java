5583870b136582b4fc239e760e355c23a371fdbc
Adamos991 <19265808@studentmail.ul.ie>
2021-11-11 19:29:19 +0000

    sign in layout established

--- app/src/main/java/com/example/freshmilk_go/MainActivity.java
+++ app/src/main/java/com/example/freshmilk_go/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.freshmilk_go;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    System.out.println("Login Successful");
+                }
+            }
+        });
     }
+
+
 }
