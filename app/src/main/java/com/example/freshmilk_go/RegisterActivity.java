61034c1050032e69208979b65650d9082aa381bc
student <19265808@studentmail.ul.ie>
2021-11-19 14:35:02 +0000

    changed more shtuff

--- app/src/main/java/com/example/freshmilk_go/RegisterActivity.java
+++ app/src/main/java/com/example/freshmilk_go/RegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -64,6 +65,12 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
         if(emailV.isEmpty()) {
             email.setError("Email is needed!");
+            email.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailV).matches()) {
+            email.setError("That's not a real email you silly goose!");
             email.requestFocus();
             return;
         }
